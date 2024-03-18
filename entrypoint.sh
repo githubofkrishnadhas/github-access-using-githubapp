bcc89cc5aae9fc83b18c4d1bbcbd97e53fe04ba0
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2024-03-19 02:01:08 +0530

    DEVOPS-46

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@
 cd /app && pipenv install --skip-lock
 
 # run python program to generate token
-pipenv run python3 /app/generate_jwt.py --github_app_id $1 --github_app_private_key $2
+pipenv run python3 /app/generate_jwt.py --github_app_id "$1" --github_app_private_key "$2"
 
 
