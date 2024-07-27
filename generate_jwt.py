6ab7dbba50446c296ef13ed804b901d041bbd885
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2024-07-27 13:35:22 +0530

    DEVOPS-265 generate jwt and access token updated py filr

--- generate_jwt.py
+++ generate_jwt.py
@@ -2,6 +2,7 @@ from jwt import JWT, jwk_from_pem
 import time
 import argparse
 import os
+import sys
 import requests
 from dotenv import load_dotenv
 
@@ -36,54 +37,84 @@ def create_jwt(private_key, app_id):
     # print(f"JWT token created successfully")
     return encoded_jwt
 
-def get_app_installation_id(jwt:str, github_account_type:str):
+def get_app_installation_id(jwt:str, owner:str, repositories:str):
     """
     returns github app installation id on user and org accounts
+    github app can be installed on scope of org providing permission to all repos or hand picked ones.
+    in either case github app is installed on organization has a unique installation id. same for user accounts as well.
     :param jwt:
     :return:
     """
-    GITHUB_REPOSITORY = os.getenv('GITHUB_REPOSITORY')
-    GITHUB_REPOSITORY_OWNER =  os.getenv('GITHUB_REPOSITORY_OWNER')
-    org_url = f'https://api.github.com/repos/{GITHUB_REPOSITORY}/installation'
-    user_url = f'https://api.github.com/users/{GITHUB_REPOSITORY_OWNER}/installation'
-    if github_account_type == 'user':
-        url = user_url
-    else:
-        url = org_url
+    results = []
+    # Pagination query params
+    per_page = 50
+    page = 1
+    # Api call url
+    url = f'https://api.github.com/app/installations'
+    # header
     headers = {
         "Accept": "application/vnd.github+json",
         "Authorization": f"Bearer {jwt}",
         "X-GitHub-Api-Version": "2022-11-28"
-    }
-    response = requests.get(url= url, headers=headers)
+        }
+    while True:
+        params = {'per_page': per_page, 'page': page}
+        response = requests.get(url= url, headers=headers, params=params)
 
-    if response.status_code == 200:
-        print(f'Okay. Received proper response.Got installation id')
-        response_json = response.json()
-    elif response.status_code == 301:
-        print(f'Moved permanently. Cant get a response')
-    else:
-        print(f'Resource Not Found!')
+        if response.status_code == 200:
+            response_json = response.json()
+        elif response.status_code == 301:
+            print(f'Moved permanently. Cant get a response')
+        else:
+            print(f'Resource Not Found!')
 
-    # Installation id of github app
-    installation_id = response_json['id']
+        # Add the current page of results to the results list
+        results.extend(response_json)
+
+        # Check if there are more results
+        if len(response_json) < per_page:
+            break
+        page += 1
+
+    # Iterating through all installations
+    for result in results:
+        result_owner = result['account']['login']
+        if owner == result_owner:
+            installation_id = result['id']
+            print(f'Installation id is {installation_id} - {owner} {result["target_type"]} ')
+            print(f'Okay. Received proper response.Got installation id')
     return installation_id
 
-def generate_token_by_post_call(installation_id:int, jwt:str):
+
+def generate_token_by_post_call(installation_id:int, jwt:str, repositories: str):
     """
     create a app installation token by doing a rest api post call with permissions for application
     :return:
     """
+    input_repositories = [item.strip() for item in repositories.split(',')]
     url = f'https://api.github.com/app/installations/{installation_id}/access_tokens'
     headers = {
-        "Accept": "application/vnd.github+json",
-        "Authorization": f"Bearer {jwt}",
+         "Accept": "application/vnd.github+json",
+           "Authorization": f"Bearer {jwt}",
         "X-GitHub-Api-Version": "2022-11-28"
     }
-    response = requests.post(url=url, headers=headers)
+    data = {
+        "repositories": input_repositories
+    }
+    response = requests.post(url=url, headers=headers, json=data)
     response_json = response.json()
     if response.status_code == 201:
-        print(f'Github app installation token generate succcessfully, expires at {response_json["expires_at"]}')
+        print(f'Github app installation token generated successfully for scope {repositories} - expires at {response_json["expires_at"]}')
+    elif response.status_code == 401:
+        print(f'Authentication is required')
+    elif response.status_code == 403:
+        print(f'Forbidden action')
+    elif response.status_code == 404:
+        print(f'Resource not found')
+    else:
+        print(f"Validation failed, {response_json['message']} or the endpoint has been spammed. Provided input repositories are {repositories}")
+        print(f'Aborting GitHub App installation token generation')
+        sys.exit(1) # Exiting as rest api cant process the req 422 error code
     os.environ['GH_APP_TOKEN'] = response_json['token']
     # Write the token to GITHUB_ENV to be available in subsequent steps
     with open(os.environ['GITHUB_ENV'], 'a') as fh:
@@ -97,18 +128,41 @@ def main():
     load_dotenv()
     parser = argparse.ArgumentParser(description="Create JWT for GitHub App authentication")
     parser.add_argument("--github_app_private_key",required=True, type=str, help="Github App Private key")
-    parser.add_argument("--github_account_type",required=True, choices=['user','organization'], help="Github account whether user account ot github org")
     parser.add_argument("--github_app_id",required=True, type=str, help="Your GitHub App ID")
+    parser.add_argument("--owner", required=False, type=str, help="Target github owner")
+    parser.add_argument("--repositories", required=False, type=str, help="Repos to which token will be generated, for multiple seperate by comma")
     args = parser.parse_args()
 
     private_key = args.github_app_private_key
     app_id = args.github_app_id
-    github_account_type = args.github_account_type
+
+    # Handle --owner argument
+    if args.owner is None:
+        print("No owner specified. Considering current repository owner as Owner")
+        owner = os.getenv('GITHUB_REPOSITORY_OWNER')  # Assign a default or dynamically determined value
+        print(f"Taking owner as {owner}")
+    else:
+        owner = args.owner
+        print(f"Owner: {owner}")
+
+    # Handle --repositories argument
+    if args.repositories is None:
+        if args.owner is not None:
+            # If owner is provided but repositories are not
+            print(f"No repositories specified for the provided owner: {owner}.")
+            repositories = 'all'  # You can decide on a default behavior here, e.g., an empty list or a specific action
+            print(f"Selecting all repos under owner {owner}")
+        else:
+            print("No repositories specified. Considering current repository as repositories")
+            repositories = os.getenv('GITHUB_REPOSITORY')  # Assign a default or dynamically determined value
+    else:
+        repositories = args.repositories
+        print(f"Will generate tokens for {repositories}")
 
     # function call
     jwt = create_jwt(private_key=private_key, app_id=app_id)
-    installation_id = get_app_installation_id(jwt=jwt, github_account_type=github_account_type)
-    generate_token_by_post_call(installation_id=installation_id, jwt=jwt)
+    installation_id = get_app_installation_id(jwt=jwt, owner=owner, repositories=repositories)
+    generate_token_by_post_call(installation_id=installation_id, jwt=jwt, repositories=repositories)
 
 
 
