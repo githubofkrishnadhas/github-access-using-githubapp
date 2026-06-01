752e1a6c6833148b1599392e1828b6e4ee1b9896
Krishnadhas N K <108367225+githubofkrishnadhas@users.noreply.github.com>
2026-06-01 22:40:41 +0530

    Devops 384 action upgrade (#37)
    * feat: update the action to uv and python 3.13
    
    * feat: update the Dockerfile and entrypoint script
    
    * feat: update the Dockerfile
    
    * fix: jwt set up from pvt key

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # installng pipenv and creating pipenv venv
-cd /app && pipenv install --skip-lock
+cd /app && uv sync && uv tree
 
 # Capture arguments
 GITHUB_APP_ID="$1"
@@ -10,7 +10,8 @@ OWNER="$3"
 REPOSITORIES="$4"
 
 # Build the command based on available parameters
-CMD="pipenv run python3 /app/generate_jwt.py --github_app_id \"$GITHUB_APP_ID\" --github_app_private_key \"$GITHUB_APP_PRIVATE_KEY\""
+uv --version
+CMD="uv run python3 /app/generate_jwt.py --github_app_id \"$GITHUB_APP_ID\" --github_app_private_key \"$GITHUB_APP_PRIVATE_KEY\""
 
 if [ -n "$OWNER" ]; then
     CMD="$CMD --owner \"$OWNER\""
